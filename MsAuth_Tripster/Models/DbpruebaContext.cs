40678d7b85f81984603bba78d6b6a909d7f944c0
dsrodriguezse <dsrodriguezse@unal.edu.co>
2023-10-10 11:44:31 -0500

    Conexión con la DB en Models

---
+++ MsAuth_Tripster/Models/DbpruebaContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MsAuth_Tripster.Models;
+
+public partial class DbpruebaContext : DbContext
+{
+    public DbpruebaContext()
+    {
+    }
+
+    public DbpruebaContext(DbContextOptions<DbpruebaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+        /*
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=127.0.0.1; DataBase=dbprueba;user id = sa;password=12345678; Trusted_Connection=False; TrustServerCertificate=True;");
+        */
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PK__Usuario__5B65BF97BDA7CD97");
+
+            entity.ToTable("Usuario");
+
+            entity.Property(e => e.Clave)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.NombreUsuario)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MsAuth_Tripster/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MsAuth_Tripster.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string? NombreUsuario { get; set; }
+
+    public string? Clave { get; set; }
+}
