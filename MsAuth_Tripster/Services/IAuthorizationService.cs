aec954ab0010154ce4888bb723a426406a78f206
dsrodriguezse <dsrodriguezse@unal.edu.co>
2023-10-10 12:25:05 -0500

    Interfaz Servicio de autorización

---
+++ MsAuth_Tripster/Services/IAuthorizationService.cs
@@ -0,0 +1,9 @@
+﻿using MsAuth_Tripster.Models.Custom;
+
+namespace MsAuth_Tripster.Services
+{
+    public interface IAuthorizationService
+    {
+        Task<AuthorizationResponse> DevolverToken(AuthorizationRequest authorization)
+    }
+}
