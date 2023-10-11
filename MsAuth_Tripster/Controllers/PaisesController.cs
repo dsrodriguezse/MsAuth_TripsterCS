116d019e9a939fccd9fa72686a33163d3941defb
dsrodriguezse <dsrodriguezse@unal.edu.co>
2023-10-10 19:33:12 -0500

    Configuración de controladores y solicitudes http

---
+++ MsAuth_Tripster/Controllers/PaisesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MsAuth_Tripster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var listaPaises = await Task.FromResult(new List<string>
+            {
+                "Francia", "Argentina", "Colombia", "España"
+            });
+            return Ok(listaPaises);
+        }
+    }
+}
---
+++ MsAuth_Tripster/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using MsAuth_Tripster.Models.Custom;
+using MsAuth_Tripster.Services;
+
+namespace MsAuth_Tripster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly IAuthorizationService _authorizationService;
+
+        public UsuarioController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+        [HttpPost]
+        [Route("Autenticar")]
+        public async Task<IActionResult> Autenticar([FromBody]AuthorizationRequest authorization)
+        {
+            var resultado_autorizacion = await _authorizationService.DevolverToken(authorization);
+            if(resultado_autorizacion == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(resultado_autorizacion);
+        }
+    }
+}
